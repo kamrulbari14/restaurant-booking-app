22469110d1e0cee8b060d4bbefe259b246099bd4
Kamrul Bari <69103450+kamrulbari14@users.noreply.github.com>
2022-11-29 18:26:18 +0600

    project initiate

---
+++ src/main/java/com/rbw/restaurantbookingapp/RestaurantBookingAppApplication.java
@@ -0,0 +1,13 @@
+package com.rbw.restaurantbookingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestaurantBookingAppApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/rbw/restaurantbookingapp/RestaurantBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbw.restaurantbookingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantBookingAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
